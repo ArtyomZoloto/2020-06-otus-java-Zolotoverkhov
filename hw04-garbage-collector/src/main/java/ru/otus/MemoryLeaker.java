6f67b8eb8a55420085db6733a9d619e914d6e5a4
Artem Zolotoverkhov <artyomzolotoverkhov@gmail.com>
2020-09-03 19:04:11 +0300

    HW04: +Calculation time.

--- hw04-garbage-collector/src/main/java/ru/otus/MemoryLeaker.java
+++ hw04-garbage-collector/src/main/java/ru/otus/MemoryLeaker.java
@@ -7,18 +7,21 @@ import java.util.Random;
 
 public class MemoryLeaker {
     public static void main(String[] args) throws Exception {
+        long was = System.currentTimeMillis();
         Random random = new Random();
         List<Date> list = new LinkedList<>();
-        while (true) {
+        int j = 0;
+        while (j++ < 50_000) {
             int i = 0;
             while (i++ < 1000) {
                 list.add(new Date());
                 list.add(new Date());
             }
-            while (i++ < 1900) {
+            while (i++ < 1999) {
                 list.remove(random.nextInt(10));
             }
-            Thread.sleep(10);
         }
+        System.out.println("done!");
+        System.out.println(System.currentTimeMillis() - was);
     }
 }
