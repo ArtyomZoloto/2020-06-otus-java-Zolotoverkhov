3283d060678c0c652f0e104eddc76da17942cc41
Artem Zolotoverkhov <artyomzolotoverkhov@gmail.com>
2020-11-21 16:45:06 +0300

    hw08 init

---
+++ hw08-json-object-writer/src/main/java/ru/otus/MyGson.java
@@ -0,0 +1,12 @@
+package ru.otus;
+
+public class MyGson {
+
+    /**
+     * Create json form object
+     * @param object input object
+     */
+    public toJson(Object object) {
+        
+    }
+}
